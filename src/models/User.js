44accf64ee5ccdf872178545adfb6e786dfcc5bf
ChenH0ng <chenh0ng@outlook.com>
2017-02-23 22:03:44 +0800

    bug fixed

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,16 @@
 export default class User {
+    get synopsis(){
+        return{
+            _id:this._id,
+            nickname:this.nickname,
+            avatar:this.avatar,
+        }
+    }
     constructor({_id, username = null, nickname = '', password = null, avatar = '',}) {
         this._id = _id;
         this.username = username;
         this.nickname = nickname;
         this.password = password;
         this.avatar = avatar;
-        this.synopsis = {
-            _id,
-            nickname,
-            avatar,
-        };
     }
 }
